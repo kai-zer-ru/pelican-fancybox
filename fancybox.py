b9b92bfecb72762e3e43f256a7e147fc3e7edad3
kaizer <kaizer@kai-zer.ru>
2016-06-14 00:51:20 +0800

    v 0.1

--- fancybox.py
+++ fancybox.py
@@ -30,9 +30,27 @@ def fancybox(generator):
 	generator.fancybox += '<script>'
 	generator.fancybox += '$( document ).ready(function(){activate_fancybox();$(".fancybox").fancybox();});'
 	generator.fancybox += '</script>'
-	
+	generator.fancybox += '<link rel="stylesheet" href="/css/fancybox.css" type="text/css"/>'	
 	generator._update_context(['fancybox'])
 
+
+def copy(pelican, writer):
+	cp_js(pelican, writer)
+	cp_css(pelican, writer)
+
+def cp_css(pelican, writer):
+	src = path.join(path.dirname(__file__), "static", "css")
+	dest = path.join(pelican.settings['OUTPUT_PATH'], 'css')
+	try:
+		shutil.rmtree(dest)
+	except OSError:
+		pass
+	try:
+		shutil.copytree(src, dest)
+	except OSError:
+		error('''Copy "%s" to "%s" does not work.''' % (
+			src, dest
+		))
 
 def cp_js(pelican, writer):
     src = path.join(path.dirname(__file__), "static", "js")
@@ -51,4 +69,4 @@ def cp_js(pelican, writer):
 def register():
     signals.initialized.connect(initialized)
     signals.article_generator_finalized.connect(fancybox)
-    signals.article_writer_finalized.connect(cp_js)
+    signals.article_writer_finalized.connect(copy)
